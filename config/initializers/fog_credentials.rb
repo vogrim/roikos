eff699b733774e20dd27ad0a80e921a0b4333a88
Franz Dumfart <s.nepomuk@gmail.com>
2014-07-03 12:43:58 +0200

    load image config only on production;

--- config/initializers/fog_credentials.rb
+++ config/initializers/fog_credentials.rb
@@ -1,8 +1,10 @@
 CarrierWave.configure do |config|
-  config.fog_credentials = {
-    :provider               => 'AWS',
-    :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
-    :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],
-  }
-  config.fog_directory  = 'roikos-crm'
+  if Rails.env.production?
+    config.fog_credentials = {
+      :provider               => 'AWS',
+      :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
+      :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+    config.fog_directory  = 'roikos-crm'
+  end
 end
