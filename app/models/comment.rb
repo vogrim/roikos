77addb72caa9d34a6e99976bbddad0a965c678be
Franz Dumfart <s.nepomuk@gmail.com>
2014-08-13 14:54:10 +0200

    formatting client detail page;

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,10 @@
 class Comment < ActiveRecord::Base
+
   belongs_to :commentable, :polymorphic => true
   belongs_to :user
 
   validates :content, presence: true
+
+  default_scope { order('created_at DESC') }
+
 end
