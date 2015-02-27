8ccc1a1cb257a079eedbabec92fa3f077a77dc02
Franz Dumfart <s.nepomuk@gmail.com>
2015-02-27 16:15:02 +0100

    validation movement type;

--- app/models/movement.rb
+++ app/models/movement.rb
@@ -3,7 +3,7 @@ class Movement < ActiveRecord::Base
   belongs_to :product
   belongs_to :movement_type
 
-  validates_presence_of :movement_at, :count, :product_id
+  validates_presence_of :movement_at, :count, :product_id, :movement_type_id
 
   default_scope { order('movement_at DESC') }
 
